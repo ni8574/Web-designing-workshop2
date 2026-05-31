c4693becd0eb10bb2cb5092f8eb6cafb98c29e50
Niharika saini <niharikasaini766@gmail.com>
2026-05-31 12:43:54 +0530

    practical.my app

---
+++ Practical-2/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import react from 'react'
+import Navbar from './components/Navbar'
+
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Practical-2/my-app/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+import Home from '../pages/Home';
+
+
+function Navbar(){
+return (
+     <div className="container">
+        <div onclick="">Home</div>
+        <div>About</div>
+        <div>Contact</div>
+     
+     </div>
+)
+}
+
+export default Navbar
+//Navbar file code
